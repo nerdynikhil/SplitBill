bf3a4ad67ea005fb9c2acc5ad63c0349041d554d
Nikhil Barik <36399086+nerdynikhil@users.noreply.github.com>
2021-08-13 07:54:25 +0530

    implement max-heap

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+import { BinaryHeap } from './heap.js';
+
 onload = function () {
-    // create the network
-    var curr_data;
-    var container = document.getElementById('mynetwork');
-    var container2 = document.getElementById('mynetwork2');
-    var genNew = document.getElementById('generate-graph');
-    var solve = document.getElementById('solve');
-    var temptext = document.getElementById('temptext');
+    // create a network
+    let curr_data;
+    const container = document.getElementById('mynetwork');
+    const container2 = document.getElementById('mynetwork2');
+    const genNew = document.getElementById('generate-graph');
+    const solve = document.getElementById('solve');
+    const temptext = document.getElementById('temptext');
     // initialise graph options
-    var options = {
+    const options = {
         edges: {
             arrows: {
                 to: true
@@ -26,29 +28,34 @@ onload = function () {
             icon: {
                 face: 'FontAwesome',
                 code: '\uf183',
-                size: 50,  //50,
+                size: 50,
                 color: '#991133',
             }
         }
     };
-    // initialize the network
-    var network = new vis.Network(container);
+    // initialize your network!
+    let network = new vis.Network(container);
     network.setOptions(options);
-    var network2 = new vis.Network(container2);
+    let network2 = new vis.Network(container2);
     network2.setOptions(options);
 
     function createData() {
-        sz = Math.floor(Math.random() * 8) + 2;
-        nodes = [];
-        for (i = 1; i <= sz; i++) {
+        const sz = Math.floor(Math.random() * 8) + 2;
+
+        // Adding people to nodes array
+        let nodes = [];
+        for (let i = 1; i <= sz; i++) {
             nodes.push({ id: i, label: "Person " + i })
         }
         nodes = new vis.DataSet(nodes);
 
-        edges = [];
-        for (i = 1; i <= sz; i++) {
-            for (j = i + 1; j <= sz; j++) {
+        // Dynamically creating edges with random amount to be paid from one to another friend
+        const edges = [];
+        for (let i = 1; i <= sz; i++) {
+            for (let j = i + 1; j <= sz; j++) {
+                // Modifies the amount of edges added in the graph
                 if (Math.random() > 0.5) {
+                    // Controls the direction of cash flow on edge
                     if (Math.random() > 0.5)
                         edges.push({ from: i, to: j, label: String(Math.floor(Math.random() * 100) + 1) });
                     else
@@ -56,16 +63,17 @@ onload = function () {
                 }
             }
         }
-        data = {
+        const data = {
             nodes: nodes,
             edges: edges
         };
-        curr_data = data;
+        return data;
     }
 
     genNew.onclick = function () {
-        createData();
-        network.setData(curr_data);
+        const data = createData();
+        curr_data = data;
+        network.setData(data);
         temptext.style.display = "inline";
         container2.style.display = "none";
     };
@@ -73,39 +81,50 @@ onload = function () {
     solve.onclick = function () {
         temptext.style.display = "none";
         container2.style.display = "inline";
-        solvedData = solveData();
-        network2.setData(solveData());
+        const solvedData = solveData();
+        network2.setData(solvedData);
     };
 
     function solveData() {
-        data = curr_data;
-        sz = data['nodes'].length;
-        vals = Array(sz).fill(0);
-        for (i = 0; i < data['edges'].length; i++) {
-            edge = data['edges'][i];
+        let data = curr_data;
+        const sz = data['nodes'].length;
+        const vals = Array(sz).fill(0);
+        // Calculating net balance of each person
+        for (let i = 0; i < data['edges'].length; i++) {
+            const edge = data['edges'][i];
             vals[edge['to'] - 1] += parseInt(edge['label']);
             vals[edge['from'] - 1] -= parseInt(edge['label']);
         }
-        for (i = 0; i < sz; i++)
-            console.log(vals[i]);
-        console.log('\n');
 
-        new_edges = [];
-        for (i = 0; i < sz; i++) {
+        const pos_heap = new BinaryHeap();
+        const neg_heap = new BinaryHeap();
+
+        for (let i = 0; i < sz; i++) {
             if (vals[i] > 0) {
-                for (j = 0; j < sz && vals[i] > 0; j++) {
-                    if (vals[j] < 0) {
-                        if (vals[j] + vals[i] >= 0) {
-                            new_edges.push({ from: j + 1, to: i + 1, label: String(Math.abs(vals[j])) });
-                            vals[i] += vals[j];
-                            vals[j] = 0;
-                        } else {
-                            new_edges.push({ from: j + 1, to: i + 1, label: String(vals[i]) });
-                            vals[j] += vals[i];
-                            vals[i] = 0;
-                        }
-                    }
-                }
+                pos_heap.insert([vals[i], i]);
+            } else {
+                neg_heap.insert(([-vals[i], i]));
+                vals[i] *= -1;
+            }
+        }
+
+        const new_edges = [];
+        while (!pos_heap.empty() && !neg_heap.empty()) {
+            const mx = pos_heap.extractMax();
+            const mn = neg_heap.extractMax();
+
+            const amt = Math.min(mx[0], mn[0]);
+            const to = mx[1];
+            const from = mn[1];
+
+            new_edges.push({ from: from + 1, to: to + 1, label: String(Math.abs(amt)) });
+            vals[to] -= amt;
+            vals[from] -= amt;
+
+            if (mx[0] > mn[0]) {
+                pos_heap.insert([vals[to], to]);
+            } else if (mx[0] < mn[0]) {
+                neg_heap.insert([vals[from], from]);
             }
         }
 
@@ -117,4 +136,5 @@ onload = function () {
     }
 
     genNew.click();
+
 };
